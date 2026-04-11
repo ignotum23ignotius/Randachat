d9c59cab40f097fb62c0a9dcf241176bbd23392c
ignotumignotius23 <ignotumignotius23@gmail.com>
2026-04-11 06:37:17 -0500

    completed: vite PWA /app subpath

--- randanonchat/client/vite.config.js
+++ randanonchat/client/vite.config.js
@@ -4,6 +4,8 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/app/',
+
   plugins: [
     react(),
 
@@ -42,21 +44,21 @@ export default defineConfig({
         name: 'RandAnonChat',
         short_name: 'RandAnon',
         description: 'Anonymous encrypted random chat',
-        start_url: '/',
-        scope: '/',
+        start_url: '/app/',
+        scope: '/app/',
         display: 'standalone',
         orientation: 'portrait',
         background_color: '#000000',
         theme_color: '#000000',
         icons: [
           {
-            src: '/icons/icon-192.png',
+            src: '/app/icons/icon-192.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'any maskable'
           },
           {
-            src: '/icons/icon-512.png',
+            src: '/app/icons/icon-512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
